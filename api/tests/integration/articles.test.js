379c40419a475735890a9b67c1d48f1be288fb6b
Schwartz, Blake <Blake.H.Schwartz.-ND@disney.com>
2017-08-11 14:43:29 -0700

    forgot this

--- api/tests/integration/articles.test.js
+++ api/tests/integration/articles.test.js
@@ -92,8 +92,8 @@ async function createNewUser(overrides) {
 /*
 http://ws.kcd.im/?ws=Testing&e=API%20Integration&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(true).toBe(submitted)
 })
 ////////////////////////////////
